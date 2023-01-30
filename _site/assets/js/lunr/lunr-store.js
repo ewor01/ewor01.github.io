de81182912adb142512fa428c7c483ac2fbf2267
root <root@localhost.localdomain>
2023-01-29 22:51:00 -0600

    update post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Blue - Hack The Box",
-        "excerpt":"Certificaciones a aplicar: OSCP Dificultad: Fácil Habilidades: SMB Enumeration Eternalblue Exploitation (MS17-010) [d4t4s3c Exploit] Obtaining credentials stored in memory [MIMIKATZ + Windows Defender Evasion] (EXTRA) Enabling RDP from CrackMapExec (EXTRA) Windows Persistence techniques (EXTRA) Windows Persistence - Playing with debugger [When a user opens a program] (EXTRA) Windows Persistence -...","categories": ["hackthebox","OSCP","Fácil","Windows"],
+        "excerpt":"Certificaciones a aplicar: OSCP Dificultad: Fácil Habilidades: SMB Enumeration Eternalblue Exploitation (MS17-010) [d4t4s3c Exploit] Obtaining credentials stored in memory [MIMIKATZ + Windows Defender Evasion] (EXTRA) Enabling RDP from CrackMapExec (EXTRA) Windows Persistence techniques (EXTRA) Persistence + Windows Defender Evasion [Playing with Ebowla] (EXTRA) IP de la Maquina 10.10.10.40 Reconocimiento Identificando...","categories": ["hackthebox","OSCP","Fácil","Windows"],
         "tags": ["SMB Enumeration","Eternalblue Exploitation(MS17-010)","Persistence","Windows Defender Evasion"],
         "url": "http://localhost:4000/blue-hackthebox/",
         "teaser":"http://localhost:4000/assets/images/htb-writeup-blue/Blue_htb.png"},{
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Ingeniería social y cómo protegerse La ingeniería social consiste en aquellas técnicas que permitan manipular a una persona para lograr que esta proporcione información relevante o lleve a cabo una acción propuesta por el atacante. Es muy importante aprender sobre la ingeniería social ya que es uno de los ataques...","categories": ["phishing","smishing","baiting"],
         "tags": ["Ingeniería Social"],
         "url": "http://localhost:4000/ingenier%C3%ADa-social/",
-        "teaser":"http://localhost:4000/assets/images/ingenier%C3%ADa-social/ingenier%C3%ADa-social.jpg"}]
+        "teaser":"http://localhost:4000/assets/images/ingenier%C3%ADa-social/ingenier%C3%ADa-social.jpg"},{
+        "title": "Jerry - Hack The Box",
+        "excerpt":"Certificaciones a aplicar: eJPT Dificultad: Fácil Habilidades: Information Leakage Abusing Tomcat [Intrusion &amp; Privilege Escalation] IP de la Maquina 10.10.10.95 Reconocimiento Identificando el sistema operativo. whichSystem.py 10.10.10.95 Escaneo de los puertos y guardamos en formato grep en el ficherito allPorts. nmap -p- --open -sS --min-rate 5000 -vvv -n -Pn 10.10.10.95...","categories": ["hackthebox","eJTP","Fácil","Windows"],
+        "tags": ["Information Leakage","Tomcat"],
+        "url": "http://localhost:4000/jerry-hackthebox/",
+        "teaser":"http://localhost:4000/assets/images/htb-writeup-jerry/Jerry.png"}]
